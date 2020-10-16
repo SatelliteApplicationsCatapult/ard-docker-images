f676a11396191c7c96602ace4f722839da2d8ea5
Luigi Di Fraia <luigidifraia@gmail.com>
2020-10-16 11:32:07 +0000

    Add the use of timeout_decorator and logging for timestamps

--- sentinel-1-backscatter-am/worker-s1-am.py
+++ sentinel-1-backscatter-am/worker-s1-am.py
@@ -4,9 +4,11 @@
 # ARD workflow #
 ################
 
+import timeout_decorator
 import json
 from utils.prepS1AM import prepareS1AM
 
+@timeout_decorator.timeout(10800, use_signals=False)
 def process_scene(json_data):
     loaded_json = json.loads(json_data)
     prepareS1AM(**loaded_json)
@@ -15,23 +17,34 @@ def process_scene(json_data):
 # Job processing #
 ##################
 
+import os
+import logging
 import rediswq
 
-import os
-host = os.getenv("REDIS_SERVICE_HOST", "redis-master")
+level = os.getenv("LOGLEVEL", "INFO").upper()
+logging.basicConfig(format="%(asctime)s %(levelname)-8s %(message)s", datefmt="%Y-%m-%d %H:%M:%S", level=level)
 
+host = os.getenv("REDIS_SERVICE_HOST", "redis-master")
 q = rediswq.RedisWQ(name="jobS1AM", host=host)
-print("Worker with sessionID: " +  q.sessionID())
-print("Initial queue state: empty=" + str(q.empty()))
+
+logging.info("Worker with sessionID: " +  q.sessionID())
+logging.info("Initial queue state: empty=" + str(q.empty()))
+
 while not q.empty():
   item = q.lease(lease_secs=1800, block=True, timeout=600) 
   if item is not None:
     itemstr = item.decode("utf=8")
-    print("Working on " + itemstr)
-    #time.sleep(10) # Put your actual work here instead of sleep.
-    process_scene(itemstr)
+    logging.info("Working on " + itemstr)
+
+    try:
+      process_scene(itemstr)
+    except timeout_decorator.TimeoutError:
+      logging.info("Timed out while working on " + itemstr)
+      pass
+
     q.complete(item)
   else:
-    print("Waiting for work")
-print("Queue empty, exiting")
+    logging.info("Waiting for work")
+
+logging.info("Queue empty, exiting")
 
