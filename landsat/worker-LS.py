a025dfd2c0c9c330796523c86786f8ea0ae8b306
Luigi Di Fraia <luigidifraia@users.noreply.github.com>
2020-10-21 15:58:09 +0100

    ICS-383 align logging across all workers (#8)
    Align logging logic across all workers

--- landsat/worker-LS.py
+++ landsat/worker-LS.py
@@ -15,23 +15,35 @@ def process_scene(json_data):
 # Job processing #
 ##################
 
-import rediswq
-
 import os
-host = os.getenv("REDIS_SERVICE_HOST", "redis-master")
+import logging
+import rediswq
+import datetime
 
+level = os.getenv("LOGLEVEL", "INFO").upper()
+logging.basicConfig(format="%(asctime)s %(levelname)-8s %(name)s %(message)s", datefmt="%Y-%m-%d %H:%M:%S", level=level)
+
+host = os.getenv("REDIS_SERVICE_HOST", "redis-master")
 q = rediswq.RedisWQ(name="jobLS", host=host)
-print("Worker with sessionID: " +  q.sessionID())
-print("Initial queue state: empty=" + str(q.empty()))
+
+logger = logging.getLogger("worker")
+logger.info(f"Worker with sessionID: {q.sessionID()}")
+logger.info(f"Initial queue state: empty={q.empty()}")
+
 while not q.empty():
-  item = q.lease(lease_secs=1800, block=True, timeout=600) 
-  if item is not None:
-    itemstr = item.decode("utf=8")
-    print("Working on " + itemstr)
-    #time.sleep(10) # Put your actual work here instead of sleep.
-    process_scene(itemstr)
-    q.complete(item)
-  else:
-    print("Waiting for work")
-print("Queue empty, exiting")
+    item = q.lease(lease_secs=1800, block=True, timeout=600)
+    if item is not None:
+        itemstr = item.decode("utf=8")
+        logger.info(f"Working on {itemstr}")
+        start = datetime.datetime.now().replace(microsecond=0)
+
+        process_scene(itemstr)
+        q.complete(item)
+
+        end = datetime.datetime.now().replace(microsecond=0)
+        logger.info(f"Total processing time {end - start}")
+    else:
+        logger.info("Waiting for work")
+
+logger.info("Queue empty, exiting")
 
