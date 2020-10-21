18c2c9f2e86066b1ed4017472f41f6cc719cfdca
Luigi Di Fraia <luigidifraia@gmail.com>
2020-10-21 10:30:26 +0000

    Show the total processing time upon completing a job

--- sentinel-1-backscatter-am/worker-s1-am.py
+++ sentinel-1-backscatter-am/worker-s1-am.py
@@ -20,6 +20,7 @@ def process_scene(json_data):
 import os
 import logging
 import rediswq
+import datetime
 
 level = os.getenv("LOGLEVEL", "INFO").upper()
 logging.basicConfig(format="%(asctime)s %(levelname)-8s %(name)s %(message)s", datefmt="%Y-%m-%d %H:%M:%S", level=level)
@@ -28,31 +29,35 @@ host = os.getenv("REDIS_SERVICE_HOST", "redis-master")
 q = rediswq.RedisWQ(name="jobS1AM", host=host)
 
 logger = logging.getLogger("worker")
-logger.info("Worker with sessionID: " +  q.sessionID())
-logger.info("Initial queue state: empty=" + str(q.empty()))
+logger.info(f"Worker with sessionID: {q.sessionID()}")
+logger.info(f"Initial queue state: empty={q.empty()}")
 
 while not q.empty():
-  item = q.lease(lease_secs=1800, block=True, timeout=600) 
-  if item is not None:
-    itemstr = item.decode("utf=8")
-    logger.info("Working on " + itemstr)
+    item = q.lease(lease_secs=1800, block=True, timeout=600)
+    if item is not None:
+        itemstr = item.decode("utf=8")
+        logger.info(f"Working on {itemstr}")
+        start = datetime.datetime.now().replace(microsecond=0)
 
-    # In case the COG conversion gets stuck, a TimeoutError is raised and we try again: the existance of a COG from an earlier iteration is often enough to progress upon retrying
-    for x in range(0, 2):  # try 2 times
-      e = False
-      try:
-        process_scene(itemstr)
-        e = True
-      except timeout_decorator.TimeoutError:
-        logger.info("Timed out while working on " + itemstr)
-        e = False
-        pass
-      if e:
-        break
+        # In case the COG conversion gets stuck, a TimeoutError is raised and we try again: the existance of a COG from an earlier iteration is often enough to progress upon retrying
+        for x in range(0, 2):  # try 2 times
+            e = False
+            try:
+                process_scene(itemstr)
+                e = True
+            except timeout_decorator.TimeoutError:
+                logger.info(f"Timed out while working on {itemstr}")
+                e = False
+                pass
+            if e:
+                break
 
-    q.complete(item)
-  else:
-    logger.info("Waiting for work")
+        q.complete(item)
+
+        end = datetime.datetime.now().replace(microsecond=0)
+        logger.info(f"Total processing time {end - start}")
+    else:
+        logger.info("Waiting for work")
 
 logger.info("Queue empty, exiting")
 
